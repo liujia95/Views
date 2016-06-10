e6b44842cf273ef83476dc86fc1a9dd753f52aa8
liujia95 <liujia95me@126.com>
2016-06-10 13:39:04 +0800

    Revert "对现有控件进行扩展："
    This reverts commit e6b225a5b40f767afb1f8ff1122b5de4bbb57941.

--- app/src/main/java/me/liujia95/views/base/BaseActivity.java
+++
@@ -1,9 +0,0 @@
-package me.liujia95.views.base;
-
-import android.support.v7.app.AppCompatActivity;
-
-/**
- * Created by Administrator on 2016/6/10 12:00.
- */
-public class BaseActivity extends AppCompatActivity {
-}
